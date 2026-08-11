c1176397316bb304764f39b220feafce40539245
musisu <mariyasinetc2007@gmail.com>
2026-08-11 15:05:27 +0300

    Refactor ApplicationForm states
    Removed old state variables and added 'username' state.

--- bot/states.py
+++ bot/states.py
@@ -2,11 +2,5 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class ApplicationForm(StatesGroup):
-    name = State()
-    age = State()
-    contact = State()
-    rp_experience = State()
-    motivation = State()
-    desired_role = State()
-    extra = State()
+    username = State()
     confirm = State()
